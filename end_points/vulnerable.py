c922cff7f1c4b353bc099a5ada6a99c0c0329cb6
joel-briones-B-Low <joellbriones701@gmail.com>
2025-01-30 15:59:31 -0600

    Acabado_2

--- end_points/vulnerable.py
+++ end_points/vulnerable.py
@@ -1,23 +1,43 @@
 from flask import Blueprint, request, jsonify
 from decoradores.decorador_cors import *
 import requests
+import os
+from urllib.parse import urlparse
+
 
 vulnerable = Blueprint('vulnerable', __name__)
+
+
 @vulnerable.route('/consulta', methods=['POST'])
 @corsPublico
 def peticion():
     try:
         url = request.json.get('url')
         
-        # Realiza la solicitud GET a la URL
         response = requests.get(url)
 
-        # Verifica si la respuesta fue exitosa
         if response.status_code == 200:
             return jsonify({'data': response.text, 'status_code': response.status_code}), 200
         else:
             return jsonify({'error': 'Error al hacer la solicitud al servidor remoto'}), response.status_code
 
     except requests.exceptions.RequestException as e:
-        # Maneja cualquier error de la solicitud
+        return jsonify({'error': str(e)}), 500
+
+
+@vulnerable.route('/consulta/archivo', methods=['POST'])
+def leer():
+    try:
+        url = request.json.get('url')
+        parsed_url = urlparse(url)  # Analiza la URL
+        nombre = parsed_url.path.lstrip("/")  # Elimina el prefijo /
+        archivo = os.path.join(os.getcwd(), 'static/js', nombre + '.js')  # Ruta al archivo
+
+        if os.path.exists(archivo):
+            with open(archivo, 'r') as file:
+                contenido = file.read()
+            return jsonify({'data': contenido, 'status_code': 200}), 200
+        else:
+            return jsonify({'error': 'El archivo no existe'}), 404
+    except Exception as e:
         return jsonify({'error': str(e)}), 500
--- static/js/vulnerable.js
+++ static/js/vulnerable.js
@@ -1,36 +1,59 @@
-    fetch('http://127.0.0.1:8080/consulta', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            url: 'http://localhost:8080/usuarios'  
-        })
+fetch('http://127.0.0.1:8080/consulta', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        url: 'http://localhost:8080/usuarios'
     })
+})
     .then(response => response.json())
-    .then(data => {console.log(data)})
+    .then(data => { console.log(data) })
     .catch(error => console.error('Error:', error));
-    document.addEventListener('DOMContentLoaded', () => {
-        const boton = document.querySelector('#boton');
-        const id = 1; // Define el id antes de agregar el listener al botón
-        
-        boton.addEventListener('click', () => eliminarUsuario(id)); 
-        
-        function eliminarUsuario(id) {
-            fetch(`http://127.0.0.1:8080/usuario/eliminar/${id}`, { 
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.querySelector('#boton');
+    const url = document.querySelector('#url');
+    const id = 2; // Define el id antes de agregar el listener al botón
+
+    boton.addEventListener('click', () => eliminarUsuario(id));
+    url.addEventListener('click', () => leerurl());
+
+    function eliminarUsuario(id) {
+        fetch(`http://127.0.0.1:8080/usuario/eliminar/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
             .then(response => {
                 if (response.ok) {
-                    console.log(`Usuario con ID ${id} eliminado correctamente`); 
+                    console.log(`Usuario con ID ${id} eliminado correctamente`);
                 } else {
                     console.error('Error al eliminar el usuario');
                 }
             })
             .catch(error => console.error('Error:', error));
-        }
-    });
-    
+    }
+
+    function leerurl() {
+        fetch('http://127.0.0.1:8080/consulta/archivo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                url: '/usuarios'
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status_code === 200) {
+                console.log('Contenido del archivo usuarios.js:', data.data);
+            } else {
+                console.error('Error:', data.error);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+        
+    }
+});
