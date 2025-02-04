d0915b15f9cf98be47140360066ecf4f61f9fdff
joel-briones-B-Low <joellbriones701@gmail.com>
2025-02-03 18:16:18 -0600

    3 vulnerabilidades

--- end_points/vulnerable.py
+++ end_points/vulnerable.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+"""from flask import Blueprint, request, jsonify
 from decoradores.decorador_cors import *
 import requests
 import os
@@ -18,6 +18,77 @@ def peticion():
 
         if response.status_code == 200:
             return jsonify({'data': response.text, 'status_code': response.status_code}), 200
+        else:
+            return jsonify({'error': 'Error al hacer la solicitud al servidor remoto'}), response.status_code
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@vulnerable.route('/consulta/archivo', methods=['POST'])
+def leer():
+    try:
+        url = request.json.get('url')
+        parsed_url = urlparse(url)  # Analiza la URL
+        nombre = parsed_url.path.lstrip("/")  # Elimina el prefijo /
+        archivo = os.path.join(os.getcwd(), 'static/js', nombre + '.js')  # Ruta al archivo
+
+        if os.path.exists(archivo):
+            with open(archivo, 'r') as file:
+                contenido = file.read()
+            return jsonify({'data': contenido, 'status_code': 200}), 200
+        else:
+            return jsonify({'error': 'El archivo no existe'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500"""
+        
+        
+        
+from flask import Blueprint, request, jsonify
+from decoradores.decorador_cors import *
+import requests
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+
+vulnerable = Blueprint('vulnerable', __name__)
+lugar = os.getcwd()
+
+def logs(cliente, lugar):
+    os.system('cls')
+    print(lugar)
+    archivo = open('logs.txt', 'a')
+    archivo.write(f'cliente: {cliente} __ lugar:  {lugar}')
+    archivo.close()
+
+@vulnerable.route('/consulta', methods=['POST'])
+def peticion():
+    try:
+        url = request.json.get('url')
+        
+        response = requests.get(url)
+        """
+        lo primero que podemos hacer para no evidenciar los js ni nada por el estilo de archivos clave es
+        solo retornar lo queno sos importe
+        lo segundo que podemos hacer es restringir los dominios:
+        lo otro que podemos hacer es monitoreos        
+        """
+        cliente = request.remote_addr
+        lugar = url
+        logs(cliente, lugar)
+        print()
+        dominios = ['https://joelBriones.com']
+        if url not in dominios:
+            return jsonify({'error': 'El dominio no es permitido'}), 403
+        if response.status_code == 200:
+            texto = response.text
+            convertido = BeautifulSoup(texto, 'html.parser')
+            contenido = convertido.body
+            texto = contenido.decode_contents()
+            
+            texto = texto[:-47]
+            return jsonify({'data': texto, 'status_code': response.status_code}), 200
         else:
             return jsonify({'error': 'Error al hacer la solicitud al servidor remoto'}), response.status_code
 
--- static/js/vulnerable.js
+++ static/js/vulnerable.js
@@ -1,22 +1,15 @@
-fetch('http://127.0.0.1:8080/consulta', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        url: 'http://localhost:8080/usuarios'
-    })
-})
-    .then(response => response.json())
-    .then(data => { console.log(data) })
-    .catch(error => console.error('Error:', error));
+
 document.addEventListener('DOMContentLoaded', () => {
     const boton = document.querySelector('#boton');
     const url = document.querySelector('#url');
+    const htmlBoton = document.querySelector('#html');
+    const contenidoUsuario = document.querySelector('#contenidoUsuario');
     const id = 2; // Define el id antes de agregar el listener al botón
 
     boton.addEventListener('click', () => eliminarUsuario(id));
     url.addEventListener('click', () => leerurl());
+    htmlBoton.addEventListener('click', () => getUsuarioHtml())
+
 
     function eliminarUsuario(id) {
         fetch(`http://127.0.0.1:8080/usuario/eliminar/${id}`, {
@@ -33,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             })
             .catch(error => console.error('Error:', error));
-    }
+    } // fin eliminarUsuairo
 
     function leerurl() {
         fetch('http://127.0.0.1:8080/consulta/archivo', {
@@ -45,15 +38,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 url: '/usuarios'
             })
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status_code === 200) {
-                console.log('Contenido del archivo usuarios.js:', data.data);
-            } else {
-                console.error('Error:', data.error);
-            }
+            .then(response => response.json())
+            .then(data => {
+                if (data.status_code === 200) {
+                    console.log('Contenido del archivo usuarios.js:', data.data);
+                } else {
+                    console.error('Error:', data.error);
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    } // fin del url
+
+    function getUsuarioHtml() {
+        fetch('http://127.0.0.1:8080/consulta', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                url: 'http://localhost:8080/usuarios'
+            })
         })
-        .catch(error => console.error('Error:', error));
-        
+            .then(response => response.json())
+            .then(response => {
+                limpiarUsuarios()
+                const {data} = response
+                console.log(data);
+                contenidoUsuario.insertAdjacentHTML("beforeend", data);
+
+                                
+            })
+            .catch(error => console.error('Error:', error));
     }
+
+        function limpiarUsuarios(){
+            while(contenidoUsuario.firstChild){
+                contenidoUsuario.removeChild(contenidoUsuario.firstChild);
+            }
+        }
+
 });
